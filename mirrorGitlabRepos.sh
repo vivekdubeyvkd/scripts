7754cba67373cb63c701bb7d695db2009ac180b1
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-02-22 14:29:13 +0530

    Create mirrorGitlabRepos.sh

---
+++ mirrorGitlabRepos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export gitlab_token=<gitlabToken>
+rm -rf repolist
+jenkins_secret=<jenkins_secret>
+wget --quiet -O - --auth-no-challenge --no-check-certificate --http-user <username> --http-password $jenkins_secret <Jenkins Job Url>/lastSuccessfulBuild/artifact/repolist > repolist
+
+cat repolist|sort|uniq| while read line
+do
+ echo "***** $line *******"
+ repoName=$(echo $line|cut -d',' -f1)
+ repoUrl=$(echo $line|cut -d',' -f2)
+ projectId=$(echo $line|cut -d',' -f3)
+ test -d repo_dir && rm -rf repo_dir
+ 
+ # Gitlab api to create a project on Gitlab named ${repoName} inside appropriate group
+ curl --header "PRIVATE-TOKEN: ${gitlab_token}" -H "Content-Type: application/json" -d '{"name”:”<name of repo>","namespace_id”:<gitlab_group_id>}' -X POST "<gitlabUrl>/api/v4/projects"
+
+ git clone --bare ${repoUrl} repo_dir
+ cd repo_dir
+ git push --mirror <gitlab_url>:<gitlab_group_name>/${repoName}.git
+ cd ..
+  
+ echo "++++++ archiving project ${repoName} ++++++++"
+ curl -s -k --request POST --header "PRIVATE-TOKEN: $gitlab_token" "<gitlab_url>/api/v4/projects/${projectId}/archive"
+ 
+done
