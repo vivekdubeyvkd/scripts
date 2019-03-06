bba6b036c4f85e7243b17a791499cf1b0ef97303
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-03-06 11:47:31 +0530

    Create executeFunctionOnRemoteServer.sh

---
+++ executeFunctionOnRemoteServer.sh
@@ -0,0 +1,12 @@
+# esnure that machine from where you are executing this has passwordless ssh connection in place if you are using this to automate something
+
+export remote_user="<username>"
+export remote_server="<remote server name or IP>"
+
+function remoteFunction() {
+    value=$1
+    echo "Hi , I am exceuting on remote machine"
+    echo $value
+}
+
+ssh "${remote_user}"@"${remote_server}" "$(typeset -f remoteFunction hello);cleanUp hello"
