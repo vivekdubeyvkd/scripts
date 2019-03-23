ad0ae06b6d011d4a8c6afbfaa7eaef6a5b81be98
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-03-24 01:46:30 +0530

    Create removeOldFilesExceptLatestNFiles.sh

---
+++ removeOldFilesExceptLatestNFiles.sh
@@ -0,0 +1,41 @@
+function removeOldFilesExceptLatestNFiles(){
+    if [ $# -ne 3 ]
+    then
+       echo " ++++++++++++++++ error +++++++++++++++"
+       echo "Wrong parameters passed ...... exiting now, please run again with correct parameters ...."
+       echo "<function> <root clean up dir> <file extension to be removed> <number of latest files to keep>"
+       exit 1
+    fi
+
+    rootCleanupDir=$1;shift
+    filePatternToBeCleanedUp=$1;shift
+    keepLatestNFiles=$1
+    totalMatchingFileCount=$(ls -lrt ${rootCleanupDir}/*${filePatternToBeCleanedUp} | wc -l )
+    fileToBeRemoved=0
+    
+    if [ ${totalMatchingFileCount} -gt ${keepLatestNFiles} ]
+    then
+        fileToBeRemoved=$((${totalMatchingFileCount} - ${keepLatestNFiles}))
+        echo "Files to be removed : ${fileToBeRemoved}" 
+        ls -lrt ${rootCleanupDir}/*${fileExtensionToBeCleanedUp} | awk '{print $NF}' | while read line
+        do
+            echo $line
+        done 
+    else
+        echo "Files to be removed : ${fileToBeRemoved}" 
+        echo "There are no older files to be removed"
+        exit 0
+    fi
+}
+
+
+rootCleanupDir="/tmp" # this is the root dir where clean needs to be done
+fileExtensionToBeCleanedUp=".txt" # file pattern to be matched and removed
+keepLatestNFiles=10
+
+# calling this with defined parameters
+removeOldFilesExceptLatestNFiles $rootCleanupDir $fileExtensionToBeCleanedUp $keepLatestNFiles
+
+# it can be called as many times as we want with different set of parameters
+removeOldFilesExceptLatestNFiles "/var/log" ".log" 20 
+removeOldFilesExceptLatestNFiles "/tmp" ".tmp" 5 
