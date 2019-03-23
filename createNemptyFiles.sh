eb014bfc7005f942777d40a3e6207eaa18011273
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-03-24 00:12:49 +0530

    Create createNemptyFiles.sh

---
+++ createNemptyFiles.sh
@@ -0,0 +1,17 @@
+# create 100 empty files as 1.txt ..... 100.txt
+
+filePrefix=".txt"
+
+# using for loop
+for i in {1..100}
+do
+  touch ${i}.${filePrefix}
+done
+
+# using while loop
+n=1
+while [ $n -le 100 ]
+do
+  touch ${i}.${filePrefix}
+  n=`expr $n + 1`
+done
