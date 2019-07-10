9e57af19f7e8e77156b5fd6c2f9e42742dcee7ea
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-07-10 15:32:37 +0530

    Update checkTheTLSVersion.sh

--- checkTheTLSVersion.sh
+++ checkTheTLSVersion.sh
@@ -1,3 +1,4 @@
+## use your hostname and port on which you want to check the acceptance of TLS requests
 openssl s_client -connect <hostname>:<port> -tls1
 openssl s_client -connect <hostname>:<port>  -tls1_1
 openssl s_client -connect <hostname>:<port> -tls1_2
