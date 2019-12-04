43479c2fb037a1cae697e1a8bf73ac1caf2bdab2
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-12-04 13:20:00 +0530

    Create open-port-commands.sh

---
+++ open-port-commands.sh
@@ -0,0 +1,9 @@
+# run it as a root
+# 55555 is port to be opened
+# reference https://www.thegeekdiary.com/how-to-open-a-ports-in-centos-rhel-7/
+
+# firewall-cmd --zone=public --add-port=55555/tcp --permanent
+success
+
+# firewall-cmd --reload
+success
